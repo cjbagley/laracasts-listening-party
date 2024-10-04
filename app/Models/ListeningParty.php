c87914ed2e12bfc4c6bdf733c4634ff48d2bc13e
Colin Bagley <cjbagley@googlemail.com>
2024-10-04 10:20:53 +0100

    fix: Larastan missing casted attribute information and erroring

--- app/Models/ListeningParty.php
+++ app/Models/ListeningParty.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\ListeningParty
+ *
+ * @property Carbon $start_time
+ * @property Carbon $end_time
+ * @property bool $is_active
+ */
 class ListeningParty extends Model
 {
-    /** @use HasFactory<\Database\Factories\ListeningPartyFactory> */
     use HasFactory;
 
     protected $guarded = ['id'];
