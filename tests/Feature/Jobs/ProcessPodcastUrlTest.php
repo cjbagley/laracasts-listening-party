b3cde4ee6d98050040e4a17336d3b355168fb8cb
Colin Bagley <cjbagley@googlemail.com>
2024-10-04 10:25:27 +0100

    fix: Change Episode->Podcast relationship to singular

--- tests/Feature/Jobs/ProcessPodcastUrlTest.php
+++ tests/Feature/Jobs/ProcessPodcastUrlTest.php
@@ -8,13 +8,13 @@ test('it processes RSS feed XML', function () {
     $ep = Episode::factory()->create(['title' => '']);
     $lp = ListeningParty::factory()->create(['episode_id' => $ep->id]);
 
-    expect($ep->podcasts()->count())->toBe(0);
+    expect($ep->podcast()->count())->toBe(0);
     ProcessPodcastUrl::dispatch('https://feeds.simplecast.com/sY509q85', $lp, $ep);
 
     $ep->refresh();
     $lp->refresh();
 
     expect($ep->title)->not->toBe('')
-        ->and($ep->podcasts()->count())->toBe(1)
+        ->and($ep->podcast()->count())->toBe(1)
         ->and($lp->end_time)->not->toBeNull();
 });
