f59f445e4d568acd9857dc9b00dab48668a3d4e9
Colin Bagley <cjbagley@googlemail.com>
2024-10-05 10:04:06 +0100

    feat: Add additional RSS feed parsing logic

--- app/Jobs/ProcessPodcastUrl.php
+++ app/Jobs/ProcessPodcastUrl.php
@@ -8,10 +8,13 @@ use App\Models\Podcast;
 use Carbon\CarbonInterval;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 
 class ProcessPodcastUrl implements ShouldQueue
 {
     use Queueable;
+
+    const BITRATE_KBPS = 128000;
 
     /**
      * Create a new job instance.
@@ -46,10 +49,43 @@ class ProcessPodcastUrl implements ShouldQueue
         ];
 
         $namespaces = $xml->getNamespaces(recursive: true);
-        $itunes_namespace = $namespaces['itunes'];
+        $itunes_namespace = $namespaces['itunes'] ?? null;
+        $episode_length = null;
 
-        $ep_length = $latest_episode->children($itunes_namespace)->duration;
-        $interval = CarbonInterval::createFromFormat('H:i:s', $ep_length);
+        if ($itunes_namespace) {
+            $episode_length = (string) $latest_episode->children($itunes_namespace)->duration;
+        }
+
+        if ($episode_length === null || $episode_length === '' || $episode_length === '0') {
+            $filesize = (int) $latest_episode->enclosure['length'];
+            $duration_in_seconds = ceil($filesize * 8 / self::BITRATE_KBPS);
+            $episode_length = (string) $duration_in_seconds;
+        }
+
+        /*
+         * Note: Normally I would extract this to a separate function, along
+         * with custom Exception, but in the interest of time and following
+         * along with the lesson, as left as per the video. Again, not production
+         * app and YAGNI when the focus is brushing up on Livewire Volt.
+         */
+        try {
+            if (str_contains($episode_length, ':')) {
+                $parts = explode(':', $episode_length);
+                if (count($parts) === 2) {
+                    $interval = CarbonInterval::createFromFormat('i:s', $episode_length);
+                } elseif (count($parts) === 3) {
+                    $interval = CarbonInterval::createFromFormat('H:i:s', $episode_length);
+                } else {
+                    throw new \Exception('Invalid episode_length');
+                }
+            } else {
+                $interval = CarbonInterval::seconds((int) $episode_length);
+            }
+        } catch (\Exception $exception) {
+            Log::error('Error parsing episode duration: '.$exception->getMessage());
+            $interval = CarbonInterval::hour();
+        }
+
         $end_time = $this->listeningParty->start_time->copy()->add($interval);
 
         $podcast = Podcast::updateOrCreate([
